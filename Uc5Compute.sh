99c417f44db8122e29c14c96948035e10d92ffaa
jitain1 <singhjitainj@gmail.com>
2022-12-07 23:45:20 +0530

    [Jitain]- UC5 Compute Successfully Uploaded

---
+++ Uc5Compute.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Uc1
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
+#UC2
+answer1=$(($a + $b * $c))
+
+echo "$answer1"
+
+#UC3
+answer2=$(($a * $b + $c))
+
+echo "$answer2"
+
+#UC4
+answer3=$(($c + $a / $b))
+
+echo "$answer3"
+
+#UC5
+answer4=$(($a % $b + $c))
+
+echo "$answer4"
