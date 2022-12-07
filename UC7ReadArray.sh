a4637995824c4c2b8ade662a54a652fb56b47514
jitain1 <singhjitainj@gmail.com>
2022-12-07 23:55:47 +0530

    [Jitain]- UC7 Read Dictionary Array Successfully Uploaded

---
+++ UC7ReadArray.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Uc1
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
+#UC2
+answer1=$(($a + $b * $c))
+
+echo "$answer1"
+
+#UC3
+answer2=$(($a * $b + $c))
+
+echo "$answer2"
+
+#UC4
+answer3=$(($c + $a / $b))
+
+echo "$answer3"
+
+#UC5
+answer4=$(($a % $b + $c))
+
+echo "$answer4"
+
+#UC6
+declare -A Result
+Result[0]="$answer1"
+Result[1]="$answer2"
+Result[2]="$answer3"
+Result[3]="$answer4"
+
+echo "Result=${Result[@]}"
+
+#UC7
+for ((i=0; i<4; i++));
+do
+	array[$i]=$(( ${Result[$i]} ))
+done
+
+echo "array=${array[@]}"
